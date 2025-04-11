802a252219773593d7283d5ec59a3e7785c4009a
Martinez Araneda <martie@uiowa.edu>
2025-04-11 15:31:55 -0500

    updated version

--- Subfolders.R
+++ Subfolders.R
@@ -5,6 +5,7 @@ dir.create("R/Functions")
 dir.create("Data")
 dir.create("Output")
 dir.create("Output/Data")
+dir.create("Output/Data/Model")
 dir.create("Output/Plots")
 dir.create("Output/Plots/RawData")
-
+dir.create("Output/Plots/Model")
