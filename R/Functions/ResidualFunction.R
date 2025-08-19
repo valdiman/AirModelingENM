271a081836ee3e259934fcdb72b5daa528859ddf
Martinez Araneda <martie@uiowa.edu>
2025-08-19 15:05:41 -0500

    removed it.

--- R/Functions/ResidualFunction.R
+++
@@ -1,7 +0,0 @@
-# Residual function
-
-fit_model_constCair <- function(pars, times, Xenm_obs, cair_avg, denm) {
-  sim_Xenm <- predict_Xenm_constCair(pars, times, cair_avg, denm)
-  return(sim_Xenm - Xenm_obs)
-}
-
